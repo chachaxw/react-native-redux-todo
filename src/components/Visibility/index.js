374622771bc3445c9a952cfa88b8437fe98faf6c
chacha <867571123@qq.com>
2019-04-30 23:26:04 +0800

    Add AddToDo screen adn visibility screen

---
+++ src/components/Visibility/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import ButtonFilter from '../Buttons/ButtonFilter';
+
+const Visibility = props => {
+
+	return (
+		<View style={styles.container}>
+			<ButtonFilter filter='SHOW_ALL'
+				txtStyle={styles.text}
+				activeStyle={styles.btnClicked}
+				UnactiveStyle={styles.btnUnclicked}
+				activeOpacity={0.7}
+				{...props} >
+					View All
+			</ButtonFilter>
+			<ButtonFilter filter='SHOW_ACTIVE'
+				txtStyle={styles.text}
+				activeStyle={styles.btnClicked}
+				UnactiveStyle={styles.btnUnclicked}
+				activeOpacity={0.7}
+				{...props} >
+					Active
+			</ButtonFilter>
+			<ButtonFilter filter='SHOW_COMPLETED'
+				txtStyle={styles.text}
+				activeStyle={styles.btnClicked}
+				UnactiveStyle={styles.btnUnclicked}
+				activeOpacity={0.7}
+				{...props} >
+					Completed
+			</ButtonFilter>
+			<ButtonFilter filter='SHOW_FAVORITE'
+				txtStyle={styles.text}
+				activeStyle={styles.btnClicked}
+				UnactiveStyle={styles.btnUnclicked}
+				activeOpacity={0.7}
+				{...props} >
+					Favorite
+			</ButtonFilter>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 0,
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		marginTop: 15,
+		marginBottom: 10,
+	},
+	text: {
+		margin: 5,
+		color: 'white',
+		backgroundColor: 'transparent',
+	},
+	btnClicked: {
+		borderRadius: 5,
+		backgroundColor: 'rgba(0, 0, 0, 0.4)',
+	},
+	btnUnclicked: {
+		borderRadius: 5,
+		backgroundColor: 'rgba(255, 255, 255, 0.3)',
+	}
+});
+
+export default Visibility;
