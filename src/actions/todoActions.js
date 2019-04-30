d11e9711413da609826b8f8b6ed38df26607e39b
chacha <867571123@qq.com>
2019-04-30 22:50:29 +0800

    Add main screen and edit screen

---
+++ src/actions/todoActions.js
@@ -0,0 +1,66 @@
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+export const TOGGLE_EDIT_TODO = 'TOGGLE_EDIT_TODO';
+export const TOGGLE_STAR_TODO = 'TOGGLE_STAR_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+
+// generate unique id
+const uid = () => Math.random().toString(34).slice(2);
+
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    payload: {
+      id: uid(),
+      isDone: false,
+      isEditing: false,
+      isStarred: false,
+      text
+    }
+  };
+}
+
+export function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    id
+  };
+}
+
+export function toggleStarTodo(id) {
+  return {
+    type: TOGGLE_STAR_TODO,
+    id
+  };
+}
+
+export function toggleEditTodo(id) {
+  return {
+    type: TOGGLE_EDIT_TODO,
+    id
+  }
+}
+
+export function editTodo(id, text) {
+  return {
+    type: EDIT_TODO,
+    id,
+    text
+  };
+}
+
+export function removeTodo(id) {
+  return {
+    type: REMOVE_TODO,
+    id
+  };
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  };
+}
